8c36f3828330682e41b8eec03a2ed76094178f77
Yoo Choi <dchoiboi00@gmail.com>
2019-11-25 21:10:43 -0500

    Initial Commit

---
+++ Get Rich!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get Rich!
+//
+//  Created by Daniel Choi on 11/25/19.
+//  Copyright © 2019 Daniel Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
